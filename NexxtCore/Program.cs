db8aceb9ce84b5b34e1789b176aad5cad809730b
Simon Attila <simon.attila1979@gmail.com>
2021-11-26 10:07:11 +0100

    #7 Cors develop beállítás

--- NexxtCore/Program.cs
+++ NexxtCore/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -15,7 +16,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "NexxtCore v1"); c.RoutePrefix = string.Empty; });
 }
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors(options => {
+        options.AllowAnyOrigin();
+        options.AllowAnyHeader();
+        options.AllowAnyMethod();
+    });
+}
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
