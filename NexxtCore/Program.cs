3391a16101cac689bf5955e11f5b3c06e6c44812
Simon Attila <simon.attila1979@gmail.com>
2021-11-26 10:05:58 +0100

    #6 Swagger root beállítás

--- NexxtCore/Program.cs
+++ NexxtCore/Program.cs
@@ -13,7 +13,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "NexxtCore v1"); c.RoutePrefix = string.Empty; });
 }
 
 app.UseHttpsRedirection();
