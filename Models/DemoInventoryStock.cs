02731dc71a8f53bafee72e985ea4b75c2dad6501
Simon Attila <simon.attila1979@gmail.com>
2021-11-26 13:34:08 +0100

    #10 Modellek létrehozása

---
+++ Models/DemoInventoryStock.cs
@@ -0,0 +1,13 @@
+﻿namespace Models;
+/// <summary>Stock data</summary>
+public class DemoInventoryStock
+{
+    /// <summary> Id </summary>
+    public Guid Id { get; set; }
+    /// <summary> Product Id </summary>
+    public Guid ProductId { get; set; }
+    /// <summary> Store Id </summary>
+    public Guid StoreId { get; set; }
+    /// <summary> Stock Quantity </summary>
+    public double Quantity { get; set; }
+}
---
+++ Models/DemoProductExt.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Models;
+
+/// <summary> Extra information in separate table (rarely used) </summary>
+public class DemoProductExt
+{
+    /// <summary> Id. Equals to Product.Id </summary>
+    [Key]
+    public Guid ProductId { get; set; }
+    /// <summary> Description </summary>
+    public string? Description { get; set; }
+    /// <summary> Minimum Stock </summary>
+    public double? MinimumStock { get; set; }
+}
+
