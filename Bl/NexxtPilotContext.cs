0343d466f2819f3682d267ba663ebc6821b7f100
Simon Attila <simon.attila1979@gmail.com>
2021-11-29 11:51:36 +0100

    #16 DbContext létrehozása

---
+++ Bl/NexxtPilotContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Bl;
+
+public class NexxtPilotContext : DbContext
+{
+    private string ConnectionString { get; set; }
+
+    public DbSet<Models.DemoProduct> Products { get; set; } = null!;
+
+
+    public NexxtPilotContext()
+    {
+
+        ConnectionString = Environment.GetEnvironmentVariable("POSTGRESQLCONNSTR_NexxtRdb") ?? "";
+        if (string.IsNullOrWhiteSpace(ConnectionString))
+            throw new Exception("Connection string is empty");
+        var contextName = this.GetType().Name;
+        var databaseName = contextName.Remove(contextName.Length - "Context".Length);
+        ConnectionString += ";Database=" + databaseName;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options
+            .UseNpgsql(ConnectionString);
+
+    }
+}
